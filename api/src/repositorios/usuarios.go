b17fa2cff1238fc8b22fa910a4d9835e4fdc85a7
Vininhos <viniciusrw2011@hotmail.com>
2023-04-18 23:09:00 -0300

    Desenvolvido métodos de atualizar e deletar usuário

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -96,3 +96,33 @@ func (repositorio Usuarios) BuscarPorID(ID uint64) (modelos.Usuario, error) {
 
 	return usuario, nil
 }
+
+// Atualizar altera as informações de um usuário no banco de dados.
+func (repositorio Usuarios) Atualizar(ID uint64, usuario modelos.Usuario) error {
+	statement, erro := repositorio.db.Prepare("update usuarios set nome = ?, nick = ?, email = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+// Deletar remove um usuário do banco de dados.
+func (repositorio Usuarios) Deletar(ID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from usuarios where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
