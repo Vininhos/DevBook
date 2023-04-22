66c4157fb38383ba7c95aafe93b66f9f3d518f6c
Vininhos <viniciusrw2011@hotmail.com>
2023-04-22 16:32:46 -0300

    Desenvolvido validação com Bearer token

--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -8,20 +8,20 @@ import (
 )
 
 // Logger escreve informações da requisição do router.
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		proximaFuncao(w, r)
 	}
 }
 
 // Autenticar verifica se o usuário fazendo a requisição está autenticado.
-func Autenticar(next http.HandlerFunc) http.HandlerFunc {
+func Autenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if erro := autenticacao.ValidarToken(r); erro != nil {
 			respostas.Erro(w, http.StatusUnauthorized, erro)
 			return
 		}
-		next(w, r)
+		proximaFuncao(w, r)
 	}
 }
