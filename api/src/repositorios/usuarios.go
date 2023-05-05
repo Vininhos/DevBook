01cf252c49f2679523818ae8609d1189667fd370
Vininhos <viniciusrw2011@hotmail.com>
2023-05-05 17:18:50 -0300

    Ajustado mudança de senha

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -238,14 +238,33 @@ func (repositorio Usuarios) BuscarSeguindo(usuarioID uint64) ([]modelos.Usuario,
 
 // BuscarSenha traz a senha de um usuário pelo ID.
 func (repositorio Usuarios) BuscarSenha(usuarioID uint64) (string, error) {
-	linhas, erro := repositorio.db.Query("select senha from usuarios where id = ?")
+	linha, erro := repositorio.db.Query("select senha from usuarios where id = ?", usuarioID)
 	if erro != nil {
 		return "", erro
 	}
-	defer db.Close()
+	defer linha.Close()
 
 	var usuario modelos.Usuario
 	if linha.Next() {
-		// Continuar aqui
+		if erro = linha.Scan(&usuario.Senha); erro != nil {
+			return "", erro
+		}
 	}
+
+	return usuario.Senha, nil
+}
+
+// AtualizarSenha atualiza a senha de um usuário.
+func (repositorio Usuarios) AtualizarSenha(usuarioID uint64, senha string) error {
+	statement, erro := repositorio.db.Prepare("update usuarios set senha = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(senha, usuarioID); erro != nil {
+		return erro
+	}
+
+	return nil
 }
