58bab4266d29e9c4c7bd62a3b39a0b87143b721e
Vininhos <viniciusrw2011@hotmail.com>
2023-05-06 15:20:15 -0300

    Implementado validação de campos JSON em criar publicação

--- api/src/modelos/publicacao.go
+++ api/src/modelos/publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicação representa uma publicação feita pelo usuário.
 type Publicacao struct {
@@ -11,4 +15,33 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar a publicação recebida.
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+// Validar valida se os campos Título e Conteúdo foram preenchidos corretamente.
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("o título é obrigatório e não pode estar em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("o conteúdo é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+// Formatar realiza um TrimSpace nos campos Título e Conteúdo.
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Titulo = strings.TrimSpace(publicacao.Conteudo)
 }
