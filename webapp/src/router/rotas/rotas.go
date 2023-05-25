7f4b2bf98f5b0616d6ff6e05f9ca5c8ffd1ea4e9
Vininhos <viniciusrw2011@hotmail.com>
2023-05-24 22:33:59 -0300

    criado home e ajustado paginas

--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -17,7 +17,8 @@ type Rota struct {
 // Configurar coloca todas as rotas dentro do router.
 func Configurar(router *mux.Router) *mux.Router {
 	rotas := rotasLogin
-	rotas = append(rotas, rotasUsuario...)
+	rotas = append(rotas, rotasUsuarios...)
+	rotas = append(rotas, rotaPaginaPrincipal)
 
 	for _, rota := range rotas {
 		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
